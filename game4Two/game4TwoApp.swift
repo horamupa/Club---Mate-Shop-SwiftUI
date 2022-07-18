7d2cd381844e7d7804b6b2dc34446a55256c72ab
horamupa <horamupa@gmail.com>
2022-07-18 20:09:24 +0300

    Initial Commit

---
+++ game4Two/game4TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  game4TwoApp.swift
+//  game4Two
+//
+//  Created by MM on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct game4TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
